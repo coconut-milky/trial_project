d588fec867db0305cf9e17c02929b420813d8f98
coconut-milky <wang-yi-chen@sjtu.edu.com>
2022-10-02 17:08:47 +0800

    commit files

---
+++ python_review/class_review.py
@@ -0,0 +1,15 @@
+class Dog:
+    def __init__(self,name,age):
+        self.name = name
+        self.age = age
+
+    def sit(self):
+        print(f"{self.name} is now sitting.")
+
+    def roll_over(self):
+        print(f"{self.name} rolled over!")
+
+my_dog = Dog('Willie', 6)
+
+my_dog.sit()
+my_dog.roll_over()
